29c1de50a9834f019c5b63b5e1303f84d6d0f87d
JoseAngelGuerreroInzunza <158419935+JoseAngelGuerreroInzunza@users.noreply.github.com>
2024-04-26 15:03:03 +0000

    actualizacion

--- demoOperadorTernario/DemoOperadorTernario.java
+++ demoOperadorTernario/DemoOperadorTernario.java
@@ -1,5 +1,18 @@
 package demoOperadorTernario;
 
 public class DemoOperadorTernario {
-    
+    public void demoOperadorTernario(){
+        int a=0;
+        String mensaje;
+        if( a>0)
+            mensaje = "Es mayor que 0";
+            else 
+            mensaje = "Es mayor que 0";
+
+        mensaje = a>0 ? "a es mayor que 0": "a es menor";
+        System.out.println(mensaje);
+
+        
+        //Acciones ante verdadero
+    }
 }
